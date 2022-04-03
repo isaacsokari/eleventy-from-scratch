c8ea881281a88e2f6015b452e7c0017d1b6764c3
Isaac Sokari <isaacsokari@gmail.com>
2022-04-03 15:32:12 +0100

    feat: add collections

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,30 @@
 module.exports = (config) => {
-	// Set directories to pass through to the dist folder
-	config.addPassthroughCopy('./src/images/');
-	return {
-		markdownTemplateEngine: 'njk',
-		dataTemplateEngine: 'njk',
-		htmlTemplateEngine: 'njk',
-		dir: {
-			input: 'src',
-			output: '_site',
-		},
-	};
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy('./src/images/');
+
+  // Returns work items, sorted by display order
+  config.addCollection('work', (collection) => {
+    return collection
+      .getFilteredByGlob('./src/work/*.md')
+      .sort((a, b) =>
+        Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1
+      );
+  });
+
+	config.addCollection('featuredWork', collection => {
+		return collection
+			.getFilteredByGlob('./src/work/*.md')
+			.sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1))
+			.filter(x => x.data.featured);
+	});
+
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: '_site',
+    },
+  };
 };
