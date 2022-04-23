d99b083492766d26842f27805f9105e0aa07c047
Isaac Sokari <isaacsokari@gmail.com>
2022-04-23 21:43:38 +0100

    ci: setup gulp
    - add sass and stylesheet pipeline
    - update startup scripts

--- .eleventy.js
+++ .eleventy.js
@@ -40,6 +40,8 @@ module.exports = (config) => {
     });
   });
 
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
